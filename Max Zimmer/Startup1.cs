bae5604bce3fc2b1f9dc975aed59f41db7f45620
onkelmux <ich@max-zimmer.de>
2021-03-14 20:05:52 +0100

    commit

---
+++ Max Zimmer/Startup1.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(Max_Zimmer.Startup1))]
+
+namespace Max_Zimmer
+{
+    public class Startup1
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.MapSignalR();
+        }
+    }
+}
---
+++ Max Zimmer/chat.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace Max_Zimmer
+{
+    public class chat : Hub
+    {
+        public void Send(string name, string message)
+        {
+            Clients.All.broadcastMessage(name, message);
+        }
+    }
+}
